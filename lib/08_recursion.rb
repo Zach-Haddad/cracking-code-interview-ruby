55570b8e304c38f0f79bbd8aab485dcfd45bcf6b
Zach-Haddad <Zhaddad7@gmail.com>
2017-07-13 20:17:46 -0700

    8.3

--- lib/08_recursion.rb
+++ lib/08_recursion.rb
@@ -8,3 +8,11 @@ def fib_rec(n)
 
   fib_rec(n - 1) + fib_rec(n - 2)
 end
+
+# Write a method that returns all subsets of a set.
+
+def rec_subsets(arr)
+  return [ [] ] if arr.empty?
+  last = rec_subsets(arr[0..-2])
+  last + last.map { |subarr| subarr + [arr.last] }
+end
